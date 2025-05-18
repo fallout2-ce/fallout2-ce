68e7d3e903ae1babef17e968b582c5084bd6fc3c
cambragol <mourfin@proton.me>
2025-05-18 16:39:23 +0900

    Update draw.cc

--- src/draw.cc
+++ src/draw.cc
@@ -1,6 +1,8 @@
 #include "draw.h"
 
+#include <cstring>
 #include <string.h>
+#include <vector>
 
 #include "color.h"
 #include "svga.h"
@@ -336,6 +338,55 @@ void transSrcCopy(unsigned char* dest, int destPitch, unsigned char* src, int sr
         }
         src += srcSkip;
         dest += destSkip;
+    }
+}
+
+void blitBufferToBufferStretchAndFixEdges(
+    unsigned char* src, int srcW, int srcH, int srcPitch,
+    unsigned char* dst, int dstW, int dstH, int dstPitch,
+    int numStates)
+{
+    // Temp buffer for over-stretched result (per state)
+    const int stretchW = dstW + 1;
+    const int stretchH = dstH + 1;
+    const int stretchPitch = stretchW;
+
+    std::vector<unsigned char> tempBuffer(stretchW * stretchH * numStates);
+
+    // Stretch all states to slightly larger temp buffer
+    blitBufferToBufferStretch(
+        src, srcW, srcH * numStates, srcPitch,
+        tempBuffer.data(), stretchW, stretchH * numStates, stretchPitch);
+
+    // Now copy only the top-left dstW x dstH portion per state into the final buffer
+    for (int state = 0; state < numStates; ++state) {
+        unsigned char* tempFrame = tempBuffer.data() + stretchW * stretchH * state;
+        unsigned char* finalFrame = dst + dstW * dstH * state;
+
+        for (int y = 0; y < dstH; ++y) {
+            memcpy(
+                finalFrame + y * dstW,
+                tempFrame + y * stretchPitch,
+                dstW);
+        }
+    }
+}
+
+void calculateScaledSize(int srcWidth, int srcHeight, int targetWidth, int targetHeight, int mode, int& outWidth, int& outHeight)
+{
+    if (mode == 2) {
+        // Fullscreen stretch
+        outWidth = targetWidth;
+        outHeight = targetHeight;
+    } else {
+        // Maintain aspect ratio
+        if (targetHeight * srcWidth >= targetWidth * srcHeight) {
+            outWidth = targetWidth;
+            outHeight = (targetWidth * srcHeight) / srcWidth;
+        } else {
+            outWidth = (targetHeight * srcWidth) / srcHeight;
+            outHeight = targetHeight;
+        }
     }
 }
 
