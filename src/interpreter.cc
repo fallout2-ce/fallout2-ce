75776e4b5bdd7ce159bfb8bdd92e38b82dc19558
Vlad K <phobos2077@users.noreply.github.com>
2026-03-20 18:53:03 +0100

    HOOK_COMBATDAMAGE (#322)
    * HOOK_COMBATDAMAGE
    
    * Fix compilation on 64bit systems
    
    * Fix incorrect hit location var

--- src/interpreter.cc
+++ src/interpreter.cc
@@ -3333,12 +3333,17 @@ ProgramValue::ProgramValue(int value)
 {
     opcode = VALUE_TYPE_INT;
     integerValue = value;
-};
+}
 ProgramValue::ProgramValue(Object* value)
 {
     opcode = VALUE_TYPE_PTR;
     pointerValue = value;
-};
+}
+ProgramValue::ProgramValue(Attack* value)
+{
+    opcode = VALUE_TYPE_PTR;
+    pointerValue = value;
+}
 
 bool ProgramValue::isPointer() const
 {
