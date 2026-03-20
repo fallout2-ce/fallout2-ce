55a9e3c70c716383542cc3e35ff5dc54c0c458d0
Mike Klaas <mike.klaas@gmail.com>
2026-03-20 16:54:39 -0700

    compile

--- src/game_dialog.cc
+++ src/game_dialog.cc
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
+#include <string>
 #include <vector>
 
 #include "actions.h"
--- src/inventory.cc
+++ src/inventory.cc
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #include <algorithm>
+#include <string>
 #include <vector>
 
 #include "actions.h"
