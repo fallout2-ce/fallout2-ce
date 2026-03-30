3686cfeb4eaac12c76cf6a512c8b13651de44345
Mike Klaas <mike.klaas@gmail.com>
2026-03-29 23:28:37 -0700

    Fix parameter order in metarules set_flags and set_outline.
    Also fix missing return val in message_box error path

--- src/sfall_metarules.cc
+++ src/sfall_metarules.cc
@@ -376,8 +376,8 @@ void mf_set_cursor_mode(Program* program, int args)
 
 void mf_set_flags(Program* program, int args)
 {
-    int flags = programStackPopInteger(program);
     Object* object = static_cast<Object*>(programStackPopPointer(program));
+    int flags = programStackPopInteger(program);
 
     object->flags = flags;
 
@@ -386,8 +386,8 @@ void mf_set_flags(Program* program, int args)
 
 void mf_set_outline(Program* program, int args)
 {
-    int outline = programStackPopInteger(program);
     Object* object = static_cast<Object*>(programStackPopPointer(program));
+    int outline = programStackPopInteger(program);
     object->outline = outline;
     programStackPushInteger(program, -1);
 }
@@ -674,6 +674,7 @@ void mf_message_box(Program* program, int args)
 
     const char* string = programStackPopString(program);
     if (string == nullptr || string[0] == '\0') {
+        programStackPushInteger(program, -1);
         return;
     }
 
