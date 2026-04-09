1680e885f2ea22150bbcd430499a9eb1547fa8d5
Mike Klaas <mike.klaas@gmail.com>
2026-04-08 20:52:02 -0700

    Implement obj_is_carrying_obj Sfall opcode (#362)
    Note: does not include the sfall fix to fix stack counts

--- src/sfall_opcodes.cc
+++ src/sfall_opcodes.cc
@@ -64,6 +64,34 @@ static void op_art_exists(Program* program)
     programStackPushInteger(program, artExists(fid));
 }
 
+static void op_obj_is_carrying_obj(Program* program)
+{
+    Object* itemObj = static_cast<Object*>(programStackPopPointer(program));
+    Object* invenObj = static_cast<Object*>(programStackPopPointer(program));
+
+    int count = 0;
+    if (invenObj != nullptr && itemObj != nullptr) {
+        Inventory* inventory = &(invenObj->data.inventory);
+        for (int index = 0; index < inventory->length; index++) {
+            InventoryItem* inventoryItem = &(inventory->items[index]);
+            if (inventoryItem->item == itemObj) {
+                if (inventoryItem->quantity <= 0) {
+                    debugPrint("%s: obj_is_carrying_obj found non-positive inventory quantity for item %p in owner %p",
+                        program->name,
+                        itemObj,
+                        invenObj);
+                    count = 1;
+                } else {
+                    count = inventoryItem->quantity;
+                }
+                break;
+            }
+        }
+    }
+
+    programStackPushInteger(program, count);
+}
+
 // read_byte
 static void op_read_byte(Program* program)
 {
@@ -1948,6 +1976,7 @@ void sfallOpcodesInit()
     // 0x8274 - int art_exists(int artFID)
     interpreterRegisterOpcode(0x8274, op_art_exists);
     // 0x8275 - int obj_is_carrying_obj(object invenObj, object itemObj)
+    interpreterRegisterOpcode(0x8275, op_obj_is_carrying_obj);
 
     // 0x8276 - any sfall_func0(string funcName)
     interpreterRegisterOpcode(0x8276, op_sfall_func0);
