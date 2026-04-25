b0b08ed14e703311de7d5d33a294abedea76f4a9
Mike Klaas <mike.klaas@gmail.com>
2026-04-24 23:05:40 -0700

    Avoid recursive sfall tap_key hooks

--- src/sfall_kb_helpers.h
+++ src/sfall_kb_helpers.h
@@ -9,6 +9,9 @@ bool sfall_kb_is_key_pressed(int key);
 /// Simulates pressing `key`.
 void sfall_kb_press_key(int key);
 
+/// Returns `true` when the next matching SDL key event was injected by `tap_key`.
+bool sfall_kb_consume_synthetic_key_event(int sdlScanCode, bool pressed);
+
 int sfall_kb_handle_key_pressed(int sdlScanCode, bool pressed);
 
 } // namespace fallout
