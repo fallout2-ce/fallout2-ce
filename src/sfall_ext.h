6ce6bea3090b59eedb4d71b44318113670107b00
Vlad K <phobos2077@users.noreply.github.com>
2026-05-03 01:24:30 +0200

    Sfall Saved Arrays (#431)
    * Sfall saved arrays support
    
    * Add "...all_arrays..." special case for a list of saved arrays
    
    * Adjust error message on load fail
    
    * Fix crash in GetArrayKey when index == size
    
    * chore: auto-format with clang-format
    
    * Update sfall compatibility md
    
    * Add keyIndex for Assoc Arrays for O(1) key lookups
    
    * Review feedback, more error logging
    
    * Fix build, increase ARRAY_MAX_STRING to 1024 to match sfall, fix gl_test_arrays triggering dynamic string block merge failure, prevent potential string buffer corruption on very long strings
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/sfall_ext.h
+++ src/sfall_ext.h
@@ -1,9 +1,14 @@
 #ifndef SFALL_EXT_H
 #define SFALL_EXT_H
 
+struct XFile;
+typedef XFile File;
+
 namespace fallout {
 
 void sfallLoadMods();
+bool sfallSaveGameData(File* stream);
+bool sfallLoadGameData(File* stream);
 
 } // namespace fallout
 
