f00b73b7c018ea7f4d150e16cc607c7931f8a7e7
Mike Klaas <mike.klaas@gmail.com>
2026-05-20 23:34:45 -0700

    rm FONT_MONO/FONT_MONO support

--- src/font_manager.h
+++ src/font_manager.h
@@ -9,6 +9,8 @@ extern FontManager gModernFontManager;
 
 int interfaceFontsInit();
 void interfaceFontsExit();
+// Scaled interface font drawing supports plain color text plus FONT_SHADOW.
+// Unsupported flags like FONT_MONO and FONT_UNDERLINE are ignored with a debug log.
 void interfaceFontDrawTextScaled2D(const Buffer2D& dest, int x, int y, const char* string, int color, float scale);
 int interfaceFontGetStringWidthScaled(const char* string, int color, float scale);
 
