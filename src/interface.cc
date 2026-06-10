8668faa06bf4b5c89112b2068813a7197d643a53
Mike Klaas <mike.klaas@gmail.com>
2026-06-09 23:26:56 -0700

    Fix Goris robe/disrobe animation
    Currently it isn't playing at all, since we're not doing renderPresent() during the animation loop.
    
    This fixes that.  Also, it optimizes the time the animation takes by starting it before the interface combat start/end animation.  This saves half a second or so.
    
    Finally, make Goris' animation affected by ui.anim_speed.  This should replace the fs_ hacks that several mods ship with

--- src/interface.cc
+++ src/interface.cc
@@ -1481,6 +1481,7 @@ void interfaceBarEndButtonsShow(bool animated)
         int frame = 0;
         while (frame < frameCount) {
             sharedFpsLimiter.mark();
+            tickersExecute();
 
             if (getTicksSince(time) >= delay) {
                 unsigned char* src = artGetFrameData(art, frame, 0);
@@ -1540,6 +1541,7 @@ void interfaceBarEndButtonsHide(bool animated)
 
         while (frame != 0) {
             sharedFpsLimiter.mark();
+            tickersExecute();
 
             if (getTicksSince(time) >= delay) {
                 unsigned char* src = artGetFrameData(art, frame - 1, 0);
