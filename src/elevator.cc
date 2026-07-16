6d6cecc9f83682dd73b120362cd860b09f67f006
Mike Klaas <mike.klaas@gmail.com>
2026-07-15 21:24:22 -0700

    NovaRains approach

--- src/elevator.cc
+++ src/elevator.cc
@@ -54,6 +54,7 @@ typedef struct ElevatorDescription {
 static int elevatorWindowInit(int elevator);
 static void elevatorWindowFree();
 static int elevatorGetLevelFromKeyCode(int elevator, int keyCode);
+static int elevatorGetLevelFromEscKey(int elevator, int map);
 
 // 0x43E950 grph_id_2
 static const int gElevatorFrmIds[ELEVATOR_FRM_COUNT] = {
@@ -395,12 +396,15 @@ int elevatorSelectLevel(int elevator, int* mapPtr, int* elevationPtr, int* tileP
     windowRefresh(gElevatorWindow);
 
     bool done = false;
+    bool skipGauge = false;
     int keyCode;
     while (!done) {
         sharedFpsLimiter.mark();
 
         keyCode = inputGetInput();
         if (keyCode == KEY_ESCAPE) {
+            keyCode = elevatorGetLevelFromEscKey(elevator, *mapPtr);
+            skipGauge = true;
             done = true;
         }
 
@@ -420,7 +424,7 @@ int elevatorSelectLevel(int elevator, int* mapPtr, int* elevationPtr, int* tileP
         sharedFpsLimiter.throttle();
     }
 
-    if (keyCode != KEY_ESCAPE) {
+    if (!skipGauge) {
         keyCode -= 500;
 
         if (*elevationPtr != keyCode) {
@@ -468,10 +472,6 @@ int elevatorSelectLevel(int elevator, int* mapPtr, int* elevationPtr, int* tileP
 
     elevatorWindowFree();
     touch_set_touchscreen_mode(false);
-
-    if (keyCode == KEY_ESCAPE) {
-        return -1;
-    }
 
     const ElevatorDescription* description = &(elevatorDescription[keyCode]);
     *mapPtr = description->map;
@@ -644,6 +644,17 @@ static int elevatorGetLevelFromKeyCode(int elevator, int keyCode)
         // consider use std toupper instead of & 0xFF
         if (c == (char)(keyCode & 0xFF)) {
             return index + 1;
+        }
+    }
+    return 0;
+}
+
+static int elevatorGetLevelFromEscKey(int elevator, int map)
+{
+    const ElevatorDescription* exits = gElevatorDescriptions[elevator];
+    for (int index = 0; index < ELEVATOR_LEVEL_MAX; index++) {
+        if (exits[index].map == map && exits[index].elevation == gElevation) {
+            return index;
         }
     }
     return 0;
