00c2c1a22908718a1b1f6876159603c60d8e0812
Mike Klaas <mike.klaas@gmail.com>
2026-08-10 14:12:35 -0700

    hard failures

--- src/interpreter.cc
+++ src/interpreter.cc
@@ -1196,6 +1196,10 @@ static void opConditionalOperatorGreaterThanEquals(Program* program)
         value[arg] = programStackPopValue(program);
     }
 
+    auto fatalInvalidComparison = [&value]() {
+        programFatalError("Invalid comparison: >= does not support %s and %s.", value[1].typeDebugString(), value[0].typeDebugString());
+    };
+
     switch (value[1].opcode) {
     case VALUE_TYPE_STRING:
     case VALUE_TYPE_DYNAMIC_STRING:
@@ -1215,7 +1219,7 @@ static void opConditionalOperatorGreaterThanEquals(Program* program)
             strings[0] = stringBuffers[0];
             break;
         default:
-            assert(false && "Should be unreachable");
+            fatalInvalidComparison();
         }
 
         result = strcmp(strings[1], strings[0]) >= 0;
@@ -1236,7 +1240,7 @@ static void opConditionalOperatorGreaterThanEquals(Program* program)
             result = value[1].floatValue >= (float)value[0].integerValue;
             break;
         default:
-            assert(false && "Should be unreachable");
+            fatalInvalidComparison();
         }
         break;
     case VALUE_TYPE_INT:
@@ -1255,7 +1259,7 @@ static void opConditionalOperatorGreaterThanEquals(Program* program)
             result = value[1].integerValue >= value[0].integerValue;
             break;
         default:
-            assert(false && "Should be unreachable");
+            fatalInvalidComparison();
         }
         break;
     case VALUE_TYPE_PTR:
@@ -1269,11 +1273,11 @@ static void opConditionalOperatorGreaterThanEquals(Program* program)
             result = true;
             break;
         default:
-            programFatalError("Invalid pointer comparison: >= only supports comparison to 0.");
+            fatalInvalidComparison();
         }
         break;
     default:
-        assert(false && "Should be unreachable");
+        fatalInvalidComparison();
     }
 
     programStackPushInteger(program, result);
@@ -1290,6 +1294,10 @@ static void opConditionalOperatorLessThan(Program* program)
     for (int arg = 0; arg < 2; arg++) {
         value[arg] = programStackPopValue(program);
     }
+
+    auto fatalInvalidComparison = [&value]() {
+        programFatalError("Invalid comparison: < does not support %s and %s.", value[1].typeDebugString(), value[0].typeDebugString());
+    };
 
     switch (value[1].opcode) {
     case VALUE_TYPE_STRING:
@@ -1310,7 +1318,7 @@ static void opConditionalOperatorLessThan(Program* program)
             str_ptr[0] = text[0];
             break;
         default:
-            assert(false && "Should be unreachable");
+            fatalInvalidComparison();
         }
 
         result = strcmp(str_ptr[1], str_ptr[0]) < 0;
@@ -1331,7 +1339,7 @@ static void opConditionalOperatorLessThan(Program* program)
             result = value[1].floatValue < (float)value[0].integerValue;
             break;
         default:
-            assert(false && "Should be unreachable");
+            fatalInvalidComparison();
         }
         break;
     case VALUE_TYPE_INT:
@@ -1350,11 +1358,11 @@ static void opConditionalOperatorLessThan(Program* program)
             result = value[1].integerValue < value[0].integerValue;
             break;
         default:
-            assert(false && "Should be unreachable");
+            fatalInvalidComparison();
         }
         break;
     default:
-        assert(false && "Should be unreachable");
+        fatalInvalidComparison();
     }
 
     programStackPushInteger(program, result);
@@ -1371,6 +1379,10 @@ static void opConditionalOperatorGreaterThan(Program* program)
     for (int arg = 0; arg < 2; arg++) {
         value[arg] = programStackPopValue(program);
     }
+
+    auto fatalInvalidComparison = [&value]() {
+        programFatalError("Invalid comparison: > does not support %s and %s.", value[1].typeDebugString(), value[0].typeDebugString());
+    };
 
     switch (value[1].opcode) {
     case VALUE_TYPE_STRING:
@@ -1391,7 +1403,7 @@ static void opConditionalOperatorGreaterThan(Program* program)
             strings[0] = stringBuffers[0];
             break;
         default:
-            assert(false && "Should be unreachable");
+            fatalInvalidComparison();
         }
 
         result = strcmp(strings[1], strings[0]) > 0;
@@ -1412,7 +1424,7 @@ static void opConditionalOperatorGreaterThan(Program* program)
             result = value[1].floatValue > (float)value[0].integerValue;
             break;
         default:
-            assert(false && "Should be unreachable");
+            fatalInvalidComparison();
         }
         break;
     case VALUE_TYPE_INT:
@@ -1431,26 +1443,24 @@ static void opConditionalOperatorGreaterThan(Program* program)
             result = value[1].integerValue > value[0].integerValue;
             break;
         default:
-            assert(false && "Should be unreachable");
+            fatalInvalidComparison();
         }
         break;
     // Sonora folks tend to use "object > 0" to test objects for nulls.
     case VALUE_TYPE_PTR:
         switch (value[0].opcode) {
         case VALUE_TYPE_INT:
-            if (value[0].integerValue > 0) {
-                result = (uintptr_t)value[1].pointerValue > (uintptr_t)value[0].integerValue;
-            } else {
-                // (ptr > int{0 or negative}) means (ptr != nullptr)
-                result = nullptr != value[1].pointerValue;
+            if (value[0].integerValue != 0) {
+                programFatalError("Invalid pointer comparison: > only supports comparison to 0.");
             }
+            result = nullptr != value[1].pointerValue;
             break;
         default:
-            assert(false && "Should be unreachable");
+            fatalInvalidComparison();
         }
         break;
     default:
-        assert(false && "Should be unreachable");
+        fatalInvalidComparison();
     }
 
     programStackPushInteger(program, result);
