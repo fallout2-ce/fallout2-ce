c82603f20b02a58af1e3336354f4b45e98c56392
jirik2077 <jiri.prymus@gmail.com>
2026-08-18 17:34:29 +0200

    use only the upper bounds check for programStackPopEnum<Map>

--- src/interpreter.h
+++ src/interpreter.h
@@ -398,7 +398,9 @@ template <>
 inline Map programStackPopEnum(Program* program)
 {
     int map = programStackPopInteger(program);
-    if (!mapIsValid(map)) {
+    
+    // do not use the mapIsValid here as -1 and -2 might be valid argument from the script perspective
+    if (map >= wmMapMaxCount()) {
         programPrintError("invalid map %d", map);
     }
 
